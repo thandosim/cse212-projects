dcfe0024e65ec2a1cbe456b73010a2697607b9e8
thandosim <sim24001@byui.edu>
2025-01-15 18:36:01 +0200

    finished week 2 learning activity

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -54,7 +54,7 @@
     /// </summary>
     /// <param name="value">Integer value to add to the queue</param>
     private void Enqueue(int value) {
-        _queue.Insert(0, value);
+        _queue.Add(value); //changed the isnert line here to add
     }
 
     /// <summary>
@@ -66,8 +66,8 @@
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var value = _queue[0]; //CHANGED THE INDEX HERE FROM 1 TO 0 
+        _queue.RemoveAt(0);    //CHANGED THE INDEX HERE FROM 1 TO 0 
         return value;
     }
 }
